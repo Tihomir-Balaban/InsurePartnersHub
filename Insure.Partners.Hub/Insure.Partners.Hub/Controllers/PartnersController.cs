825a5522c7d8d6ce4f4cc3a75b662429f3030c52
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-02-28 14:24:31 +0100

    feat: init controle implementations

---
+++ Insure.Partners.Hub/Insure.Partners.Hub/Controllers/PartnersController.cs
@@ -0,0 +1,23 @@
+﻿using Insure.Partners.Hub.Service.Interfaces;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Insure.Partners.Hub.Controllers
+{
+    public class PartnersController : Controller
+    {
+        private readonly IPartnerService partnerService;
+
+        public PartnersController(IPartnerService partnerService)
+        {
+            this.partnerService = partnerService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var partners = await partnerService.GetAllAsync();
+
+            return View(partners);
+        }
+    }
+}
---
+++ Insure.Partners.Hub/Insure.Partners.Hub/Controllers/PoliciesController.cs
@@ -0,0 +1,26 @@
+﻿using Insure.Partners.Hub.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Insure.Partners.Hub.Controllers
+{
+    public class PoliciesController : Controller
+    {
+        private readonly IPolicyService policyService;
+
+        public PoliciesController(IPolicyService policyRepository)
+        {
+            policyService = policyRepository;
+        }
+
+        public async Task<ActionResult> Index(int partnerId)
+        {
+            var policies = await policyService.GetByPartnerIdAsync(partnerId);
+            return View(policies);
+        }
+    }
+}
