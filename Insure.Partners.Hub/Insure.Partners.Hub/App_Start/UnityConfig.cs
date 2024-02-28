29a890362c597d9f83c3b7701a6c535f81355df5
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-02-28 13:32:44 +0100

    feat: clean up

--- Insure.Partners.Hub/Insure.Partners.Hub/App_Start/UnityConfig.cs
+++ Insure.Partners.Hub/Insure.Partners.Hub/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using Insure.Partners.Hub.Repository.Interfaces;
 using Insure.Partners.Hub.Repository.Repositories;
 using Insure.Partners.Hub.Service.Interfaces;
 using Insure.Partners.Hub.Service.Services;
+using System;
 using System.Configuration;
 using System.Web.Mvc;
 using Unity;
@@ -9,9 +10,30 @@ using Unity.AspNet.Mvc;
 
 namespace Insure.Partners.Hub
 {
-
     public static class UnityConfig
     {
+        private static Lazy<IUnityContainer> container =
+          new Lazy<IUnityContainer>(() =>
+          {
+              var container = new UnityContainer();
+              RegisterTypes(container);
+              return container;
+          });
+
+        public static IUnityContainer Container => container.Value;
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+
+            var connectionString = ConfigurationManager.AppSettings["ConnectionString"]; ;
+
+            container.RegisterType<IPartnerService, PartnerService>();
+            container.RegisterType<IPolicyService, PolicyService>();
+
+            container.RegisterType<IPartnerRepository, PartnerRepository>(connectionString);
+            container.RegisterType<IPolicyRepository, PolicyRepository>(connectionString);
+        }
+
         public static void RegisterComponents()
         {
             var connectionString = ConfigurationManager.AppSettings["ConnectionString"]; ;
